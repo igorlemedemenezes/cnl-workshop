8ce990c635466f3fb192df66922608327dd425ef
Igor Leme <igor.leme.m@outlook.com>
2019-09-14 22:56:56 -0300

    Primeiro commit

---
+++ src/main/java/com/igor/cnl/Cnl4Application.java
@@ -0,0 +1,13 @@
+package com.igor.cnl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cnl4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cnl4Application.class, args);
+	}
+
+}
